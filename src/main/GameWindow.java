e8ffd31e3d1ea355774ad31f79a1fe45fc07c324
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2023-02-28 17:47:55 +0100

    Initial commit

---
+++ src/main/GameWindow.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+
+public class GameWindow {
+    private JFrame jframe;
+    public GameWindow(GamePanel gamePanel) {
+        jframe = new JFrame();
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setResizable(false);
+        jframe.pack();
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+    }
+}
